25f8776b3076d0795a4a99f4679c021f24bcbd36
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-18 11:16:16 +0000

    exercises 12 and 13(not finished)

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,18 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+
+let list = document.getElementById('mySelect');
+
+//to the event we take the option selected
+countries.forEach( (e) => {
+    let aux = document.createElement('option')
+    aux.innerHTML = e
+    list.appendChild(aux)
+})
+
+function alerta(){
+    let valor = list.value;
+    alert(valor)
+}
+
+list.addEventListener('change', alerta)
--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,23 @@
 // Your code here
+var list = document.getElementById("list");
+let input = document.getElementById('addToDo');
+
+// add item function
+function addItem(e) {
+   var item = document.createElement("li").innerHTML = '<span><i class="fa fa-trash"></i></span> '+input.value;
+   var valor = el.value;
+   item.appendChild(valor);
+   list.appendChild(item);
+   item.addEventListener("keypress", removeItem, false);
+}
+
+// remove item function
+function removeItem(e) {
+   var item = e.currentTarget;
+   var itemRev = item.parentNode;
+   itemRev.removeChild(item);
+}
+
+var el = document.getElementById("addToDo");
+
+input.addEventListener("click", addItem, false);
